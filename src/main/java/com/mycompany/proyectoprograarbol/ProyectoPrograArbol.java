7aeb6dca73f53dc499f7cf373a6e29d07af2d314
Allan Lacan <allanvasquez1312@gmail.com>
2026-04-17 23:19:35 -0600

    segundo commit

--- src/main/java/com/mycompany/proyectoprograarbol/ProyectoPrograArbol.java
+++ src/main/java/com/mycompany/proyectoprograarbol/ProyectoPrograArbol.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.proyectoprograarbol;
 
-/**
- *
- * @author allan
- */
+
 public class ProyectoPrograArbol {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Probando David x Jhon");
     }
 }
