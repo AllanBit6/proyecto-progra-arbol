c2882c6ce9ee2203f35f72b5a17f08e3ce6efd3a
Allan Lacan <allanvasquez1312@gmail.com>
2026-04-18 22:09:01 -0600

    Se creo el prototipo de ventana principal

--- src/main/java/com/mycompany/proyectoprograarbol/ProyectoPrograArbol.java
+++ src/main/java/com/mycompany/proyectoprograarbol/ProyectoPrograArbol.java
@@ -1,10 +1,12 @@
 
 package com.mycompany.proyectoprograarbol;
+import com.mycompany.proyectoprograarbol.Views.MainView;
 
 
 public class ProyectoPrograArbol {
 
     public static void main(String[] args) {
-        System.out.println("Probando David x Jhon");
+        MainView view = new MainView();
+        view.setVisible(true);
     }
 }
